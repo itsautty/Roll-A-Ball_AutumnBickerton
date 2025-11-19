d00c21a811081d13ace82927f2c2dad2b23dd16a
Autumn Bickerton <a.xrkour@gmail.com>
2025-11-19 15:26:27 -0400

    FinalBuildSprint3

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -1,12 +1,33 @@
 using UnityEngine;
 
 public class Rotator : MonoBehaviour
+
 {
-  
+    [SerializeField] AudioSource audioSource;
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        audioSource.Play();
+        Destroy(gameObject, audioSource.clip.length);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
